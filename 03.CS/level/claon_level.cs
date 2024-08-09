3101b3d949d0c97685f65b048dd9c197c063e1e3
sung_won <81358337+heh-heh@users.noreply.github.com>
2024-08-09 15:07:15 +0900

    feat : 에니 메이션 수정

--- 03.CS/level/claon_level.cs
+++ 03.CS/level/claon_level.cs
@@ -1,35 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class claon_level : MonoBehaviour
 {
     // Start is called before the first frame update
+    public enum obj_type{
+        wall,
+        plane,
+        tower
+    };
+    public obj_type cloan_type;
+
     public GameObject clone_obj;
-    public int claon_count_x;
-    public int claon_count_y;
+    public List<int> claon_count = new List<int>();
     public Vector2 claon_pos;
-    public bool is_claon = true;
+    public Vector3 clzon_size;
     void Start()
     {
-        Vector3 [] t_pos = new Vector3[claon_count_x*claon_count_y];
+        // Quaternion [] t_rot = new 
+        int size = claon_count[0] * claon_count[1];
+        Vector3 [] t_pos = new Vector3[size];
+        Quaternion [] t_rot = new Quaternion[size];
         int ii=0;
-        if(is_claon){
-            for(int i=0; i<claon_count_x; i++){
-                for(int j=0; j<claon_count_y; j++){
+        if(cloan_type == obj_type.plane){
+            for(int i=0; i<claon_count[0]; i++){
+                for(int j=0; j<claon_count[1]; j++){
                     t_pos[ii++] = new Vector3(
                         clone_obj.transform.position.x + (claon_pos.x * i),
                         clone_obj.transform.position.y,
                         clone_obj.transform.position.z + (claon_pos.y * j));
                 }
             }
-            for(int i=1; i<claon_count_x*claon_count_y; i++){
-                    GameObject test = Instantiate(clone_obj,  t_pos[i], clone_obj.transform.rotation);
-                    claon_level test2 = test.GetComponent<claon_level>();
-                    test2.is_claon = false;
+        }
+        else if(cloan_type == obj_type.wall){
+            for(int i=0; i<claon_count[0]; i++){
+                for(int j=0; j<claon_count[1]; j++){
+                    t_pos[ii++] = new Vector3(
+                        clone_obj.transform.position.x + (claon_pos.x * i),
+                        clone_obj.transform.position.y+ (claon_pos.y * j),
+                        clone_obj.transform.position.z );
+                }
             }
         }
+        else if(cloan_type == obj_type.tower){
+            float agl = 360 / claon_count[0];
+            for(int i=0; i<claon_count[0]; i++){
+                t_rot[i] = Quaternion.Euler(new Vector3(0f,gameObject.transform.rotation.eulerAngles.y+(agl*i),0));
+                t_pos[i] = new Vector3(
+                    gameObject.transform.position.x+(claon_pos.x * MathF.Cos((t_rot[i].eulerAngles.y-90)*(Mathf.PI / 180))),
+                    gameObject.transform.position.y,
+                    gameObject.transform.position.z+(claon_pos.x * Mathf.Sin((t_rot[i].eulerAngles.y+90)*(Mathf.PI / 180)))
+                );
+            }
+        }
+            // Debug.Log(size);
+        for(int i=0; i<size; i++){
+            Debug.Log(t_pos[i]);
+            Debug.Log(t_rot[i]);
+            GameObject test = Instantiate(clone_obj,  t_pos[i], t_rot[i]);
+            test.gameObject.transform.localScale = clzon_size;
+        }
+        
     }
-
-    // Update is called once per frame
 }
